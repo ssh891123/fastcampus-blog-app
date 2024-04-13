b48124a94e7726e2800555b677d4e7cf6e856c2a
ssh891123 <ssh891123@gmail.com>
2024-04-13 16:02:15 +0900

    [refactor] 파일 경로를 상대경로에서 절대 경로로 수정
    - 상대경로는 프로젝트가 커질수록 경고 관리가 어렵고, 파일 접근하는데
      복잡해 지는 단점이 있음
    - 절대 경로 변경
      - tsconfig 옵션을 수정하여 파일 접근을 절대 경로 가져오도록 수정
      - import 하고 있는 컴포넌트 경로를 절대 경로로 수정

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Routes, Navigate, Link } from 'react-router-dom';
-import Home from '../pages/home';
-import PostsPage from '../pages/posts';
-import PostDetail from '../pages/posts/detail';
-import PostNew from '../pages/posts/new';
-import PostEdit from '../pages/posts/edit';
-import ProfilePage from '../pages/profile';
-import LoginPage from '../pages/login';
-import SignupPage from '../pages/signup';
+import Home from 'pages/home';
+import PostsPage from 'pages/posts';
+import PostDetail from 'pages/posts/detail';
+import PostNew from 'pages/posts/new';
+import PostEdit from 'pages/posts/edit';
+import ProfilePage from 'pages/profile';
+import LoginPage from 'pages/login';
+import SignupPage from 'pages/signup';
 
 export default function Router() {
   return (
