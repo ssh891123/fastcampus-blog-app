e11f6ce7c46e04692214607cb41147b30a7ca699
ssh891123 <ssh891123@gmail.com>
2024-04-13 17:05:21 +0900

    [feat] 게시글 생성과 편집 페이지 구현
    - 게시글 생성과 편집 페이지 구현

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Header from "./Header";
 
 export default function PostDetail() {
@@ -13,8 +14,10 @@ export default function PostDetail() {
                     <div className="post__date">2024.04.10 수요일</div>
                 </div>
                 <div className="post__utils-box">
-                    <div className="post__edit">수정</div>
                     <div className="post__delete">삭제</div>
+                    <div className="post__edit">
+                        <Link to={`/posts/edit/1`}>수정</Link>
+                    </div>
                 </div>
                 <div className="post__text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
