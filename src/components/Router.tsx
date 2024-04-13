7bb4a00a16fa81fba4b121247d04a665c520d200
ssh891123 <ssh891123@gmail.com>
2024-04-13 18:28:07 +0900

    [feat] 로그인, 회원 가입 화면 구성
    - 로그인, 회원가입 페이지 화면 구성
    - 로그인 인증되지 않은 상태에서는 로그인 화면만 보여주도록 routing 세팅

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes, Navigate, Link } from 'react-router-dom';
 import Home from 'pages/home';
 import PostsPage from 'pages/posts';
@@ -10,18 +10,31 @@ import LoginPage from 'pages/login';
 import SignupPage from 'pages/signup';
 
 export default function Router() {
+  // [ToDo]firebase  Auth가 인증되었으면 true로 변경해주는 로직 추가
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/posts' element={<PostsPage />} />
-        <Route path='/posts/:id' element={<PostDetail />} />
-        <Route path='/posts/new' element={<PostNew />} />
-        <Route path='/posts/edit/:id' element={<PostEdit />} />
-        <Route path='/profile' element={<ProfilePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignupPage />} />
-        <Route path="*" element={<Navigate replace to ="/" />} />
+        {isAuthenticated ? (
+          <>
+            <Route path='/' element={<Home />} />
+            <Route path='/posts' element={<PostsPage />} />
+            <Route path='/posts/:id' element={<PostDetail />} />
+            <Route path='/posts/new' element={<PostNew />} />
+            <Route path='/posts/edit/:id' element={<PostEdit />} />
+            <Route path='/profile' element={<ProfilePage />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/signup' element={<SignupPage />} />
+            <Route path="*" element={<Navigate replace to ="/" />} />
+          </>
+        ) :
+          <>
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/signup' element={<SignupPage />} />
+            <Route path="*" element={<Navigate replace to ="/login" />} />
+          </>
+        }
       </Routes>
     </>
   );
