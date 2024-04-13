65b55983c8d00aaee39b6eff0bda7f95eb0e5a4d
ssh891123 <ssh891123@gmail.com>
2024-04-13 15:34:13 +0900

    [feat] 웹 페이지 추가 및 페이지 요소 컴포넌트화
    - 페이지 추가: 프로필 페이지, 게시글 상세 페이지 추가
    - 중복으로 사용되는 페이지 요소의 컴포넌트 화 진행:  코드 재사용 목적
    -- Header, Footer 컴포넌트 추가
    -- 게시글 목록 컴포넌트 추가

---
+++ src/components/PostList.tsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+interface PostListProp {
+    hasNavigation?: boolean;
+}
+
+export default function PostList({ hasNavigation = true }: PostListProp) {
+    return (
+        <>
+            { hasNavigation && (
+                <div className="post__navigation">
+                    <div className="post__navigation--active">전체</div>
+                    <div>나의 글</div>
+                </div>
+            )}
+            <div className="post__list">
+                {[...Array(10)].map((e, index) => (
+                    <div key={index} className="post__box">
+                        <Link to={`/posts/${index}`}>
+                            <div className="post__profile-box">
+                                <div className="post__profile" />
+                                <div className="post__author-name">패스트 캠퍼스</div>
+                                <div className="post__date">2024.04.10 수요일</div>
+                            </div>
+                            <div className="post__title">게시글 {index}</div>
+                            <div className="post__text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </div>
+                            <div className="post__utils-box">
+                                <div className="post__edit">수정</div>
+                                <div className="post__delete">삭제</div>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
