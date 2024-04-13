810a72d04594681263325700f0ab156168c29071
ssh891123 <ssh891123@gmail.com>
2024-04-13 21:46:44 +0900

    [feat] 회원가입 기능 추가
    - email, password, password_confirm 확인하는 로직 구현
    -- email: 정규식 이용하여 이메일 계정 확인
    -- password: 8자리 이상  입력받도록, password_confirm 값과 동일하도록 확인
    -- password_confire: 8자리 이상 입력받도록, password 값과 동일하도록 확인
    - firebase의 email, password 값 등록
    - react-toastify 라이브러리 이용하여 회원값이 성공시 알림나오도록 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Router from './components/Router';
 import { app } from 'firebaseApp';
 import { getAuth } from 'firebase/auth';
 import { useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const auth = getAuth(app);
@@ -9,7 +11,12 @@ function App() {
 
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
-  return <Router isAuthenticated={isAuthenticated}/>;
+  return (
+    <>
+      <ToastContainer />
+      <Router isAuthenticated={isAuthenticated} />
+    </>
+  );
 }
 
 export default App;
