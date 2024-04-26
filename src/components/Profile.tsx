ac6ad9af0b7466f5938d1ecf4d5995d8f370e43d
ssh891123 <ssh891123@gmail.com>
2024-04-26 16:16:14 +0900

    [feat] context api를 이용하여 user 인증 정보 구현
    - context api를 이용하여 user 인증정보 저장
    - Profile Component에 user정보를 context api로 가져오도록 변경

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,27 +1,30 @@
-import { Link } from "react-router-dom";
+import AuthContext from "context/AuthContext";
 import { app } from 'firebaseApp';
 import { getAuth, signOut } from "firebase/auth";
 import { toast } from "react-toastify";
+import { useContext } from "react";
+
+const onSignOut = async () => {
+    try {
+        const auth = getAuth(app);
+        await signOut(auth);
+        toast.success("로그아웃 되었습니다.");
+    } catch(error: any) {
+        toast.error(error?.code);
+    }
+};
 
 export default function Profile() {
-    const auth = getAuth(app);
-    console.log('Profile', auth);
-    const onSignOut = async () => {
-        try {
-            const auth = getAuth(app);
-            await signOut(auth);
-            toast.success("로그아웃 되었습니다.");
-        } catch(error: any) {
-            toast.error(error?.code);
-        }
-    };
+    const { user } = useContext(AuthContext);
+    console.log(user);
+
     return (
         <div className="profile__box">
             <div className="flex__box-lg">
                 <div className="profile__image" />
                 <div>
-                    <div className="profile__email">{auth?.currentUser?.email}</div>
-                    <div className="profile__user">{auth?.currentUser?.displayName || '사용자'}</div>
+                    <div className="profile__email">{user?.email}</div>
+                    <div className="profile__user">{user?.displayName || '사용자'}</div>
                 </div>
             </div>
             <div role='presentation' className="profile__logout" onClick={onSignOut}>
