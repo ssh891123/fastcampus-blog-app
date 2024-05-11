39f94fb051678e9458e0743e1e69956e4fb098cc
ssh891123 <ssh891123@gmail.com>
2024-05-11 16:54:05 +0900

    [feat] 테마 icon, 토글 기능 구현
    - 테마 icon 추가: react-icon 라이브러리 이용
      - type/react-icon 라이브러리는 당장에 필요하지 않아 추가 하지 않음
    - 테마 토글 기능 추가
      - localstorage 에 저장하여 새로고침이나 다시 열였을때도 테마 상태
        유지하도록 구현
      - 테마 버튼 클릭시 토글되도록 구현

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,24 @@
 import { Link } from "react-router-dom";
+import { BsSun, BsMoonFill } from "react-icons/bs";
+import ThemeContext from "context/ThemeContext";
+import { useContext } from "react";
 
 export default function Footer() {
+    const context = useContext(ThemeContext);
+    console.log(context);
+
     return (
         <footer>
             <Link to="/posts/new">글쓰기</Link>
             <Link to="/posts">게시글</Link>
             <Link to="/profile">프로필</Link>
+            <div>
+                {context.theme === 'light' ? (
+                    <BsSun onClick={context.toggleMode} className="footer_theme-btn"/>
+                ) : (
+                    <BsMoonFill onClick={context.toggleMode} className="footer_theme-btn"/>
+                )}
+            </div>
         </footer>
     )
 }
--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -10,9 +10,11 @@ interface ThemeProps {
 }
 
 export const ThemeContextProvider = ({ children }: ThemeProps) => {
-    const [theme, setTheme] = useState("light");
+    const [theme, setTheme] = useState(window.localStorage.getItem("theme") || "light");
+    
     const toggleMode = () => {
         setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+        window.localStorage.setItem("theme", theme === 'light' ? 'dark' : 'light');
     }
 
     return (
